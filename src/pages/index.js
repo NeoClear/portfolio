97f4701b1c6079a646071ab55cf20f5e4c3a132c
NeoClear <neoclear@outlook.com>
2021-10-05 22:43:48 -0400

    remove SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Layout, SEO } from 'components/common';
+import { Layout } from 'components/common';
 import { Intro, Experience, Skills, Contact, Projects } from 'components/landing';
 
 export default () => (
   <Layout>
-    <SEO />
     <Intro />
     <Skills />
     <Experience />
