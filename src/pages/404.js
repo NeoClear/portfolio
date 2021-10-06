2da6792446958ba51129bfd232043c5c12a921c9
NeoClear <neoclear@outlook.com>
2021-10-05 22:54:11 -0400

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,9 @@
-// import React from 'react';
-// import { Layout, SEO } from 'components/common';
+import React from 'react';
+import { Layout } from 'components/common';
 
-// export default () => (
-//   <Layout>
-//     <SEO title="404: Not found" location="/404" />
-//     <h1>NOT FOUND</h1>
-//     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-//   </Layout>
-// );
+export default () => (
+  <Layout>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
+);
