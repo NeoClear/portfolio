5fd7f92017450d9a7f2afc7760d6797a57a46d7b
NeoClear <neoclear@outlook.com>
2021-10-08 18:54:39 -0400

    add contact info

--- src/components/landing/Contact/ContactForm/index.jsx
+++ src/components/landing/Contact/ContactForm/index.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Contacts, Linker } from './styles';
+import { ThemeContext } from 'providers/ThemeProvider';
 
-export default () => (
-  <div>
-    Yet to be built
-  </div>
-);
+import {
+  contact,
+} from 'data/config';
+
+export default () => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <Contacts theme={theme}>
+      <h1>
+        Contact Me
+      </h1>
+      <p>
+        Wanna discuss potential employment opportunities with me? Checkout my <Linker href={"mailto:" + contact.email + "?subject=Potential Software Developer Opportunity At <Company>"}>email</ Linker>!
+      </p>
+    </Contacts>
+  );
+};
--- src/components/landing/Contact/ContactForm/styles.js
+++ src/components/landing/Contact/ContactForm/styles.js
@@ -16,3 +16,63 @@ export const InputField = styled.div`
   position: relative;
   margin-bottom: 1rem;
 `;
+
+export const Contacts = styled.div`
+  h1 {
+    margin-bottom: 2rem;
+    font-size: 26pt;
+    color: ${({ theme }) => (theme === 'dark' ? '#fff' : '#212121')};
+
+    @media (max-width: 960px) {
+      mix-blend-mode: ${({ theme }) => (theme === 'light' ? 'unset' : 'difference')};
+    }
+  }
+
+  p {
+    margin-bottom: 2.5rem;
+    font-size: 20pt;
+    font-weight: normal;
+    line-height: 1.3;
+    color: ${({ theme }) => (theme === 'dark' ? '#c7c7c7' : '#707070')};
+
+    @media (max-width: 960px) {
+      mix-blend-mode: ${({ theme }) => (theme === 'light' ? 'unset' : 'difference')};
+    }
+  }
+`;
+
+export const Email = styled.a`
+  display: flex;
+  flex-direction: row;
+
+  img {
+    margin: 0px;
+  }
+
+  a {
+    font-size: 24;
+    color: #0074d9;
+  }
+`;
+
+export const Linker = styled.a`
+  color: #0074d9;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    background: gray;
+  }
+
+  ${({ secondary }) =>
+    secondary &&
+    `
+		background: #001F3F;
+	`}
+`;
